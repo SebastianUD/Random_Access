ef7856ece0f4b15d7fe31664e88c974ed25ba863
SebastianUD <112674146+SebastianUD@users.noreply.github.com>
2023-03-09 21:46:02 -0500

    Commit_2

--- RandomAccess/src/Control/Launcher.java
+++ RandomAccess/src/Control/Launcher.java
@@ -1,10 +1,8 @@
 package Control;
-import Modelo.Vino;
-import Vista.Interfaz;
 
 public class Launcher {
 
-    public static void main(String[] args) { //Static porque es un hilo de ejecución de todas las clases del proyecto, void porque no retorna valor, main porque es principal    
-        new Controlador();
+    public static void main(String[] args) {
+        new Control();
     }
 }
